fe3971b02d3757d4d5fbd2944b60094e91343f1d
Muhidin Hukic <muhidinhukic1@gmail.com>
2024-07-31 09:07:46 -0500

    refactored routes

--- spec/routes/roles_routes_spec.rb
+++ spec/routes/roles_routes_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require 'roda'
 require 'rack/test'
 require_relative '../../routes/roles_routes'
-require 'byebug'
 require 'json'
 
 RSpec.describe RolesRoutes do
@@ -17,72 +16,87 @@ RSpec.describe RolesRoutes do
     end.freeze.app
   end
 
-  # Clean the database before each test
+  let(:json_headers) { { 'CONTENT_TYPE' => 'application/json' } }
+
   before(:each) do
     TEST_DB[:roles].delete
   end
 
+  def json_response
+    JSON.parse(last_response.body)
+  end
+
   describe 'GET /roles' do
+    before do
+      TEST_DB[:roles].multi_insert([
+                                     { name: 'Role 1', description: 'Role 1' },
+                                     { name: 'Role 2', description: 'Role 2' }
+                                   ])
+    end
+
     it 'returns all roles' do
-      TEST_DB[:roles].multi_insert([{ name: 'Role 1', description: 'Role 1' },
-                                    { name: 'Role 2', description: 'Role 2' }])
-
       get '/roles'
-
       expect(last_response.status).to eq(200)
-      json_response = JSON.parse(last_response.body)
       expect(json_response['data'].length).to eq(2)
     end
   end
 
   describe 'GET /roles/:id' do
-    it 'returns a specific role' do
-      role_id = TEST_DB[:roles].insert(name: 'Test Role')
+    context 'when the role exists' do
+      let(:role_id) { TEST_DB[:roles].insert(name: 'Test Role') }
 
-      get "/roles/#{role_id}"
-
-      expect(last_response.status).to eq(200)
-      json_response = JSON.parse(last_response.body)
-      expect(json_response['data']['name']).to eq('Test Role')
+      it 'returns a specific role' do
+        get "/roles/#{role_id}"
+        expect(last_response.status).to eq(200)
+        expect(json_response['data']['name']).to eq('Test Role')
+      end
     end
 
-    it 'returns 404 for non-existent role' do
-      get '/roles/999999'
-
-      expect(last_response.status).to eq(404)
+    context 'when the role does not exist' do
+      it 'returns 404' do
+        get '/roles/999999'
+        expect(last_response.status).to eq(404)
+      end
     end
   end
 
   describe 'POST /roles' do
-    it 'creates a new role with valid params' do
-      post('/roles', { role: { name: 'New Role', description: 'New Role' } }.to_json,
-           'CONTENT_TYPE' => 'application/json')
-      expect(last_response.status).to eq(201)
-      json_response = JSON.parse(last_response.body)
-      expect(json_response['data']['name']).to eq('New Role')
+    context 'with valid params' do
+      let(:valid_params) { { role: { name: 'New Role', description: 'New Role' } }.to_json }
+
+      it 'creates a new role' do
+        post '/roles', valid_params, json_headers
+        expect(last_response.status).to eq(201)
+        expect(json_response['data']['name']).to eq('New Role')
+      end
     end
 
-    it 'returns errors with invalid params' do
-      post '/roles', { name: '' }.to_json, { 'CONTENT_TYPE' => 'application/json' }
+    context 'with invalid params' do
+      let(:invalid_params) { { role: { name: '' } }.to_json }
 
-      expect(last_response.status).to eq(422)
+      it 'returns errors' do
+        post '/roles', invalid_params, json_headers
+        expect(last_response.status).to eq(422)
+      end
     end
   end
 
   describe 'DELETE /roles/:id' do
-    it 'deletes an existing role' do
-      role_id = TEST_DB[:roles].insert(name: 'Role to Delete')
+    context 'when the role exists' do
+      let(:role_id) { TEST_DB[:roles].insert(name: 'Role to Delete') }
 
-      delete "/roles/#{role_id}"
-
-      expect(last_response.status).to eq(200)
-      expect(TEST_DB[:roles][id: role_id]).to be_nil
+      it 'deletes the role' do
+        delete "/roles/#{role_id}"
+        expect(last_response.status).to eq(200)
+        expect(TEST_DB[:roles][id: role_id]).to be_nil
+      end
     end
 
-    it 'returns 404 for non-existent role' do
-      delete '/roles/999999'
-
-      expect(last_response.status).to eq(404)
+    context 'when the role does not exist' do
+      it 'returns 404' do
+        delete '/roles/999999'
+        expect(last_response.status).to eq(404)
+      end
     end
   end
 end

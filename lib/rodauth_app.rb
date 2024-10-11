cee940e3e96de307d57c4ae6a2ec138745f75e33
hukic-m <muhidinhukic1@gmail.com>
2024-10-11 11:26:09 -0500

    don't call authenticated? on account hash

--- lib/rodauth_app.rb
+++ lib/rodauth_app.rb
@@ -12,7 +12,7 @@ class RodauthApp < Rodauth::Auth
     jwt_decode_opts({ verify_expiration: true })
 
     jwt_session_hash do
-      if account && account_id && account.authenticated?
+      if account && account_id && authenticated?
         acc = Account[account_id]
         super().merge({
                         'roles' => acc.roles.map(&:name),
